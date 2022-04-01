6380b61a70616839caacb2b387853b8f1ed09076
Alejandro Jimenez <ajimbar@outlook.com>
2022-04-01 10:23:56 -0600

    Creacion de Registro Socios

--- js/registroSocios.js
+++ js/registroSocios.js
@@ -1,30 +1,11 @@
-const nombreSocio = document.getElementById('txt-nombreSocio');
-const codigo = document.getElementById('txt-codigoSocio');
-
-const btnRegistrar = document.getElementById('btn-registrar');
+const btnRegistrar = document.getElementById('btnGuardar');
 
 const validarRegistroSocio = () => {
 
-    let hayError;
+    let hayError = validarFormulario();
 
 
-    let inputsTxt = document.getElementsByName('input');
 
-    //Hacemos un For, que se ejecutara mientras nuestra contador i se menor que la cantidad de inputsTexts obtenidos
-    for (let i = 0; i < inputsTxt.length; i++) {
-
-        //Validamos si el contenido del input esta vacio, de estarlo pasamos la variable de error a true y añadimos la clase de indicador de error  
-        if (inputsTxt[i].value == '') {
-
-            hayError = true
-            inputsTxt[i].classList.add('inputError')
-
-        } else {
-            //Si no esta vacio quitamos la clase de error
-            inputsTxt[i].classList.remove('inputError')
-        }
-
-    }
     //Mensaje del resultado de la validacion
     if (hayError == true) {
         Swal.fire({
