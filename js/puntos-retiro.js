c6aa901218ae4e0dce8bf9dbe1e773ceb446f887
Alejandro Jimenez <ajimbar@outlook.com>
2022-04-04 12:03:01 -0600

    Creacion de Catalogo Puntos de Retiro (js, css y html) y Reenvio de Facturas (html y css)

---
+++ js/puntos-retiro.js
@@ -0,0 +1,25 @@
+let listaPuntosRetiro = [{
+
+    'nombre': 'Walmart',
+    'imgSocio': '/media/img/socios/walmart.jpg',
+    'direccion': 'San José, Santa Ana, Complejo empresarial Forum II, Edificio K.',
+
+}, {
+    'nombre': 'Auto Mercado',
+    'imgSocio': '/media/img/socios/automercado.jpg',
+    'direccion': 'San José, Avenida 3, Calles 3 y, 5, San José',
+
+    'nombre': 'Ferreterias EPA',
+    'imgSocio': '/media/img/socios/epa.png',
+    'direccion': 'Diagonal al cruce de, Autop. Próspero Fernández, San José Province, Escazu',
+}, {
+    'nombre': 'Pricesamrt',
+    'imgSocio': '/media/img/socios/pricesmart.png',
+    'direccion': 'frente al convento de las monjas, 350 metros noreste del paso elevado de la ruta 32, San Jose, San Vicente',
+
+}, {
+    'nombre': 'Megasuper',
+    'imgSocio': '/media/img/socios/megasuper.jpg',
+    'direccion': 'Mall Paraiso,Cartago',
+
+}];
