e0a3f997bf5feb8038b393eb3aab0c79c0cf4d1a
Alejandro Jimenez <ajimbar@outlook.com>
2022-04-04 18:30:05 -0600

    Creacion de RegistroGeneros (html,css,js)

---
+++ js/registro-generos.js
@@ -0,0 +1,26 @@
+const btnRegistrar = document.getElementById('btnGuardar');
+
+const validarRegistroGenero = () => {
+
+    let hayError = validarFormulario();
+
+
+    //Mensaje del resultado de la validacion
+    if (hayError == true) {
+        Swal.fire({
+            "icon": "warning",
+            "title": "El socio comercial no se pudo registrar",
+            "text": "Compruebe que todos los campos estan correctamente llenados"
+        });
+
+    } else { //Si la variable de error termina en false esto lanza un popup al usuario para indicar registro exitoso
+        Swal.fire({
+            "icon": "success",
+            "title": "Éxito",
+            "text": "Socio Comercial registrado con exito"
+        });
+    }
+
+}
+
+btnRegistrar.addEventListener('click', validarRegistroGenero);
