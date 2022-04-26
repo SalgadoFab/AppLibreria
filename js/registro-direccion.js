3284335efad010fc6a290880612b9d949d26bf4f
Evelyn Palma <evegr22@hotmail.com>
2022-04-26 11:03:26 -0600

    se modifica campos de provincia,canton y distrito en archivo html

--- js/registro-direccion.js
+++ js/registro-direccion.js
@@ -1,5 +1,56 @@
-const btnRegistrar = document.getElementById('btnGuardar');
+const listaProvincias = document.getElementById('slt-provincias');
+const listaCantones = document.getElementById('slt-cantones');
+const listaDistritos = document.getElementById('slt-distritos');
 
+let provinciaSeleccionada,
+    cantonSeleccionado;
+
+const mostrarProvincias = () => {
+    distribucion.provincias.forEach(provincia => {
+        listaProvincias.options.add(new Option(provincia.title));
+    });
+};
+
+const mostrarCantones = (nombreProvincia) => {
+    listaCantones.innerHTML = '';
+    listaCantones.options.add(new Option('-- Seleccione un cantón --'));
+    distribucion.provincias.forEach(provincia => {
+        if (provinciaSeleccionada == provincia.title) {
+            provincia.cantones.forEach(canton => {
+                listaCantones.options.add(new Option(canton.title));
+            });
+        }
+    });
+};
+
+const mostrarDistritos = (nombreCanton) => {
+    listaDistritos.innerHTML = '';
+    listaDistritos.options.add(new Option('-- Seleccione un distrito --'));
+    distribucion.provincias.forEach(provincia => {
+        if (provinciaSeleccionada == provincia.title) {
+            provincia.cantones.forEach(canton => {
+                if (cantonSeleccionado == canton.title) {
+                    canton.distritos.forEach(distrito => {
+                        listaDistritos.options.add(new Option(distrito.title));
+                    });
+                }
+            });
+        }
+    });
+};
+
+mostrarProvincias();
+listaProvincias.addEventListener('change', () => {
+    provinciaSeleccionada = listaProvincias.value;
+    mostrarCantones(provinciaSeleccionada);
+})
+listaCantones.addEventListener('change', () => {
+    cantonSeleccionado = listaCantones.value;
+    mostrarDistritos(cantonSeleccionado);
+})
+
+
+const btnRegistrar = document.getElementById('btnGuardar');
 const validarRegistroDireccionEnvio = () => {
 
     let hayError = validarFormulario();
--- js/registro-usuarios.js
+++ js/registro-usuarios.js
@@ -10,7 +10,7 @@ const mostrarProvincias = () => {
         listaProvincias.options.add(new Option(provincia.title));
     });
 };
-    
+
 const mostrarCantones = (nombreProvincia) => {
     listaCantones.innerHTML = '';
     listaCantones.options.add(new Option('-- Seleccione un cantón --'));
@@ -58,7 +58,7 @@ const btnRegistrar = document.getElementById('btnGuardar');
 let errorRequerimientoPassword = false;
 
 //Funcion de Validacion de requerimientos de contraseña
-(function(){
+(function() {
     //Obtenemos el campo de contraseña
     let password = document.querySelector('.password');
     console.log(password)
@@ -78,27 +78,27 @@ let errorRequerimientoPassword = false;
         //Valida que la contraseña tenga 8 o mas caracteres
         longitudCaracteres: function() {
             //Valida que la contraseña tenga 8 o mas caracteres 
-            if( password.value.length >= 8 ) {
+            if (password.value.length >= 8) {
                 return true;
             }
         },
         //Valida que la contraseña contenga minusculas 
         minuscula: function() {
             //La variable requerimiento almacena el requisito a validar de la contraseña
-            let requerimiento = /^(?=.*[a-z]).+$/; 
+            let requerimiento = /^(?=.*[a-z]).+$/;
 
             //Se testea el requerimiento expresado a la contraseña
-            if( requerimiento.test(password.value) ) {
+            if (requerimiento.test(password.value)) {
                 return true;
             }
         },
         //Valida que la contraseña contenga mayusculas
         mayuscula: function() {
             //La variable requerimiento almacena el requisito a validar de la contraseña
-            let requerimiento = /^(?=.*[A-Z]).+$/; 
+            let requerimiento = /^(?=.*[A-Z]).+$/;
 
             //Se testea el requerimiento expresado a la contraseña
-            if( requerimiento.test(password.value) ) {
+            if (requerimiento.test(password.value)) {
                 return true;
             }
         },
@@ -107,28 +107,28 @@ let errorRequerimientoPassword = false;
             //La variable requerimiento almacena el requisito a validar de la contraseña
             let requerimiento = /^(?=.*[0-9_\W]).+$/; // caracter character or number patron
 
-            if( requerimiento.test(password.value) ) {
+            if (requerimiento.test(password.value)) {
                 return true;
             }
-        }   
+        }
     };
-    
+
     //Se ejecuta la validacion de la contraseña en tiempo real con cada valor ingresado al input
-    password.addEventListener('keyup', function (){
+    password.addEventListener('keyup', function() {
         //Verifica que la contraseña tenga un mínimo de 8 caracteres
-        validarPatron( patron.longitudCaracteres(), requerimientoPassword.longitudCaracteres );
-        
+        validarPatron(patron.longitudCaracteres(), requerimientoPassword.longitudCaracteres);
+
         //Verifica que la contraseña contenga una letra minúscula
-        validarPatron( patron.minuscula(), requerimientoPassword.minuscula );
-        
+        validarPatron(patron.minuscula(), requerimientoPassword.minuscula);
+
         //Verifica que la contraseña contenga una letra mayuscula
-        validarPatron( patron.mayuscula(), requerimientoPassword.mayuscula );
-        
+        validarPatron(patron.mayuscula(), requerimientoPassword.mayuscula);
+
         //Verifica que la contraseña contenga un número o caracter
-        validarPatron( patron.caracter(), requerimientoPassword.caracter );
-    
+        validarPatron(patron.caracter(), requerimientoPassword.caracter);
+
         //Valida que todos los requerimientos contengan la clase validado
-        if( validarClase(requerimientoPassword.longitudCaracteres, 'validado') && validarClase(requerimientoPassword.minuscula, 'validado') &&  validarClase(requerimientoPassword.mayuscula, 'validado') &&  validarClase(requerimientoPassword.caracter, 'validado') ) {
+        if (validarClase(requerimientoPassword.longitudCaracteres, 'validado') && validarClase(requerimientoPassword.minuscula, 'validado') && validarClase(requerimientoPassword.mayuscula, 'validado') && validarClase(requerimientoPassword.caracter, 'validado')) {
             console.log("PASSWORD Valida")
             errorRequerimientoPassword = false
             txtPassword.classList.remove("inputError");
@@ -139,11 +139,11 @@ let errorRequerimientoPassword = false;
             txtPassword.classList.add("inputError");
         }
     });
-    
+
     //Funcion de validacion reutilizable para cada requerimiento de la contraseña
     function validarPatron(patron, requerimiento) {
         //Si el patron es validado agregamos la clase validado
-        if(patron) {
+        if (patron) {
             agregarClase(requerimiento, 'validado');
         }
         //Si el patron falla removemos la clase validado
@@ -151,20 +151,19 @@ let errorRequerimientoPassword = false;
             removerClase(requerimiento, 'validado');
         }
     }
-    
+
     function agregarClase(elemento, nombreClase) {
 
         //Agregamos la clase obtenida por parametro del elemento necesario
         if (elemento.classList) {
             elemento.classList.add(nombreClase);
-        }
-        else {
+        } else {
             elemento.className += ' ' + nombreClase;
         }
     }
-    
+
     function removerClase(elemento, nombreClase) {
-        
+
         //Removemos la clase obtenida por parametro del elemento necesario
         if (elemento.classList) {
             elemento.classList.remove(nombreClase);
@@ -172,25 +171,24 @@ let errorRequerimientoPassword = false;
             elemento.className = elemento.className.replace(new RegExp('(^|\\b)' + nombreClase.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
         }
     }
-    
+
     function validarClase(elemento, nombreClase) {
 
         //Valida que el elemento contenga la clase recibida por parametro
         if (elemento.classList) {
-            return elemento.classList.contains(nombreClase);    
-        }
-        else {
-            new RegExp('(^| )' + nombreClase + '( |$)', 'gi').test(elemento.className); 
+            return elemento.classList.contains(nombreClase);
+        } else {
+            new RegExp('(^| )' + nombreClase + '( |$)', 'gi').test(elemento.className);
         }
     }
-    
+
 })();
 
 
 
 // this of eye method
 const mostrarPassword = (input, selector) => {
-    if(input.classList.contains('activo')) {
+    if (input.classList.contains('activo')) {
         input.setAttribute('type', 'text');
         selector.className = 'fa fa-eye';
         input.classList.remove('activo')
@@ -204,18 +202,18 @@ const mostrarPassword = (input, selector) => {
 mostrarPass = document.getElementById('mostrarPassword');
 mostrarPassConfirmacion = document.getElementById('mostrarPasswordConfirmacion');
 
-mostrarPass.onclick = function () {
-    mostrarPassword (txtPassword, mostrarPass)
+mostrarPass.onclick = function() {
+    mostrarPassword(txtPassword, mostrarPass)
 }
-mostrarPassConfirmacion.onclick = function () {
-    mostrarPassword (txtPasswordConfirmacion, mostrarPassConfirmacion)
+mostrarPassConfirmacion.onclick = function() {
+    mostrarPassword(txtPasswordConfirmacion, mostrarPassConfirmacion)
 }
 
 
 
 //Funcion de Validacion de campos normales
 const validarRegistroUsuario = () => {
-    
+
     //Creamos la letiable de error y le asignamos el valor que devuelva nuestra funcion de validacion de campos del formulario
     //IMPORTANTE - Esta funcion valida solo si el campo se encuentra vacio, para casos mas espesificos crear una validaciones aparte
     let errorCamposVacios = validarFormulario();
@@ -232,11 +230,11 @@ const validarRegistroUsuario = () => {
     if (errorRequerimientoPassword == true) {
         txtPassword.classList.add("inputError");
     }
-    
+
     console.log(errorCamposVacios)
 
     //Mensaje del resultado de la validacion
-    if ( (errorCamposVacios == true) || (errorRequerimientoPassword == true) ) {
+    if ((errorCamposVacios == true) || (errorRequerimientoPassword == true)) {
         Swal.fire({
             "icon": "warning",
             "title": "El usuario no se pudo registrar",
@@ -251,7 +249,7 @@ const validarRegistroUsuario = () => {
         });
     }
 
-} 
+}
 
 //Referenciamos a nuestro boton registrar con el evento click para ejecutar nuestra funcion de validacion
 btnRegistrar.addEventListener('click', validarRegistroUsuario);
